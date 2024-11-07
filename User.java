64d4fb47593aa70bd5bc646f931cac0a60b0ca1a
Sergia-B <58150720+sergiacleto@users.noreply.github.com>
2024-11-07 15:48:27 -0300

    Create User.java

---
+++ User.java
@@ -0,0 +1,38 @@
+package com.seuusuario.esseeujali.model;
+
+public class User {
+    private String username;
+    private String password;
+    private int points;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.points = 0;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
+    
